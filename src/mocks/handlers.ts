b390c81c243718779c1d54320e827ab2b4107b3a
Levin <mamboer@live.com>
2023-12-11 00:01:21 +0800

    fix: e2e test

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,10 @@
 /* eslint-disable */
-import { http } from 'msw'
+import { http, HttpResponse } from 'msw'
 import fruits from './data/fruits.json'
 
 const handlers = [
-	http.get(
-		'https://614c99f03c438c00179faa84.mockapi.io/fruits',
-		async (_, response, context) => response(context.json(fruits))
+	http.get('https://614c99f03c438c00179faa84.mockapi.io/fruits', async () =>
+		HttpResponse.json(fruits)
 	)
 ]
 
